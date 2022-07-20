0754bd1f8a0becf483a4e3353c855d51fc1c3eb2
Brandon <btphill4@asu.edu>
2022-07-20 15:00:15 -0700

    Updates to python

--- Python/Capstone Related/csvImport.py
+++ Python/Capstone Related/csvImport.py
@@ -5,8 +5,9 @@ businesses = []
 with open("/home/brandon/Desktop/business.csv", 'r') as businessFile:
     rows = csv.reader(businessFile, delimiter= ',')
     for r in rows:
-        #append(r[0]) get first column, append(r[1]) gets 2nd column
+        #append(r[0]) get first column, append(r[1]) gets 2nd column, etc
         businesses.append(r[1])
         #print(r)
 
 print(businesses)
+#print(businesses[1])
